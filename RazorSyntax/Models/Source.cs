3fc0db8e7ad65ae03537e18a0c051528b25171e1
EbramTawfik <ebram.s.tawfik@gmail.com>
2018-07-31 21:17:41 -0700

    RazorSyntax

---
+++ RazorSyntax/Models/Source.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RazorSyntax.Models
+{
+	public class Product
+	{
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public double Price { get; set; }
+	}
+
+	public class Discount
+	{
+		public DateTime Start { get; set; }
+		public DateTime End { get; set; }
+		public double Rate { get; set; }
+	}
+
+	public class ProductListVM
+	{
+		public Discount Discount { get; set; }
+		public IList<Product> Products { get; set; }
+	}
+}
