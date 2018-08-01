269eb73bf7c40d2f270d2084739cc97a9117c0d8
EbramTawfik <ebram.s.tawfik@gmail.com>
2018-07-31 22:07:06 -0700

    LayoutViewExample

---
+++ LayoutViewExample/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutViewExample.Controllers
+{
+    public class HomeController : Controller
+    {
+		public IActionResult Home() => View();
+		public IActionResult Page1() => View();
+		public IActionResult Page2() => View();
+	}
+}
