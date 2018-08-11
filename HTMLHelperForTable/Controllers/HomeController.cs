f3f681b3bbe7c0200bc0921ff0399d1141903bbc
EbramTawfik <ebram.s.tawfik@gmail.com>
2018-08-11 13:48:57 -0700

    HTML Helper for Table

---
+++ HTMLHelperForTable/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HTMLHelperForTable.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			ViewData["columnNames"] = new string[] { "ID", "Name", "Price" };
+			ViewData["content"] = new string[,]{
+	              	                              {"101", "Apple", "1.01"},
+	              	                              {"202", "Back", "2.02"},
+	            	                              {"303", "Cup", "3.03"},
+	             	                              {"404", "Donut", "3.03"}
+	                                               };
+			return View();
+		}
+	}
+}
---
+++ HTMLHelperForTable/Extensions/CustomHtmlHelper.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Html;
+
+namespace Microsoft.AspNetCore.Mvc.Rendering
+{
+	public static class MyHtmlHelperExtensions
+	{
+		public static IHtmlContent Table(this IHtmlHelper htmlHelper, string[] columnNames, string[,] content)
+		{
+			var tableBuilder = new TagBuilder("table");
+			tableBuilder.Attributes.Add("border", "1");
+			var headerBuilder = new TagBuilder("tr");
+			foreach (var cn in columnNames)
+			{
+				var headerCellBuilder = new TagBuilder("th");
+				headerCellBuilder.InnerHtml.Append(cn);
+				headerBuilder.InnerHtml.AppendHtml(headerCellBuilder);
+			}
+
+			tableBuilder.InnerHtml.AppendHtml(headerBuilder);
+			int colCount = columnNames.Length;
+			int rowCount = content.Length / colCount;
+			for (int r = 0; r < rowCount; r++)
+			{
+				var rowBuilder = new TagBuilder("tr");
+				for (int c = 0; c < colCount; c++)
+				{
+					var cellBuilder = new TagBuilder("td");
+					cellBuilder.InnerHtml.Append(content[r, c]);
+					rowBuilder.InnerHtml.AppendHtml(cellBuilder);
+				}
+				tableBuilder.InnerHtml.AppendHtml(rowBuilder);
+			}
+
+			return tableBuilder;
+		}
+	}
+}
